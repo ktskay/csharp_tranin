7210ab7f3e5cd676d3ba06033643dde705906701
Kosta Tskay <ktskay@yahoo.com>
2018-09-07 15:23:21 -0700

    last from lection 2

--- addressbook-web-tests/addressbook-web-tests/appmanager/ContactsHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ContactsHelper.cs
@@ -25,47 +25,72 @@ namespace WebAddressbookTests
 
         public ContactsHelper AddNewContact()
         {
-            driver.FindElement(By.LinkText("add new")).Click();
+            AddNewContact();
+            return this;
+        }
+
+       
+
+        public ContactsHelper Modify(int v, ContactsData newContactsData)
+        {
+            SelectContact();
+            InitContactsModification();
+            FillContactForm(newContactsData);
+            SubmitContactsModification();
+            return this;
+        }
+
+        public ContactsHelper SubmitContactsModification()
+        {
+            driver.FindElement(By.Name("update")).Click();
+            return this;
+        }
+
+        public ContactsHelper InitContactsModification()
+        {
+            driver.FindElement(By.XPath("//img[@alt='Edit']")).Click();
             return this;
         }
 
         public ContactsHelper FillContactForm(ContactsData contacts)
         {
-            driver.FindElement(By.Name("firstname")).Clear();
-            driver.FindElement(By.Name("firstname")).SendKeys(contacts.Firstname);
-            driver.FindElement(By.Name("middlename")).Clear();
-            driver.FindElement(By.Name("middlename")).SendKeys(contacts.Middlename);
-            driver.FindElement(By.Name("lastname")).Clear();
-            driver.FindElement(By.Name("lastname")).SendKeys(contacts.Lastname);
+            FillContactForm(contacts);
             return this;
         }
 
-        public ContactsHelper SubmitNewContact()
+       
+
+       public ContactsHelper SubmitNewContact()
         {
-            driver.FindElement(By.Name("submit")).Click();
+            SubmitNewContact();
             return this;
         }
 
-
+      
 
 
         public ContactsHelper SelectContact()
         {
-            driver.FindElement(By.Name("selected[]")).Click();
+            SelectContact();
             return this;
         }
+
+       
 
         public ContactsHelper DeleteContact()
         {
-            driver.FindElement(By.XPath("//input[@value='Delete']")).Click();
+            DeleteContact();
             return this;
         }
 
+      
         public ContactsHelper ConfirmedWindow()
         {
-            Assert.IsTrue(Regex.IsMatch(CloseAlertAndGetItsText(), "^Delete 1 addresses[\\s\\S]$"));
+            ConfirmedWindow();
             return this;
         }
+
+       
 
         private bool IsElementPresent(By by)
         {
---
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactsModificationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebAddressbookTests;
+
+namespace WebAddressbookTests
+{
+    [TestFixture ]
+    class ContactsModificationTests : TestBase 
+    {
+        [Test]
+        public void ContactsModificationTest()
+        {
+            
+            ContactsData newContactsData  = new ContactsData("vasek");
+            newContactsData.Middlename = "vasekovich";
+            newContactsData.Lastname = "vasilkov";
+            app.Contacts
+                .FillContactForm(newContactsData)
+                .SubmitNewContact();
+
+            app.Contacts.Modify(1, newContactsData );
+
+        }
+    }
+}
