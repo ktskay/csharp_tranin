4bf9485b764f3c9b7b75020bb36242a4f13bd98b
Kosta Tskay <ktskay@yahoo.com>
2018-09-24 16:16:01 -0700

    ese ras

--- addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
@@ -107,13 +107,7 @@ namespace WebAddressbookTests
         }
         public GroupHelper SelectGroup(int index)
         {
-            if (IsElementPresent(By.XPath("(//input[@name='selected[]'])[" + index + "]")))
-            {
-                driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]"));
-            }
-            else {
-                driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
-            }
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
             return this;
         }
         public GroupHelper RemoveGroup()
